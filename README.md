1ecba288d62de84b0eead41d59a675f4f4a9f442
SridharChallabotla <sridhar.challabotla@metlife.com>
2020-03-16 19:39:36 +0530

    commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 "# sree-springcloud-m5-dataservice1" 
 "localhost:8081/fastpass/customer/102/contactdetails" 
+localhost:8083/fastpass/customer/102
---
+++ src/main/java/sree/demo/CustomSampler.java
@@ -0,0 +1,22 @@
+/*
+ * package sree.demo;
+ * 
+ * 
+ * import org.springframework.cloud.sleuth.Sampler; import
+ * org.springframework.cloud.sleuth.Span; import
+ * org.springframework.context.annotation.Bean; import
+ * org.springframework.context.annotation.Configuration;
+ * 
+ * @Configuration public class CustomSampler {
+ * 
+ * @Bean public Sampler smartSampler() {
+ * 
+ * return new Sampler() {
+ * 
+ * @Override public boolean isSampled(Span span) {
+ * System.out.println("custom sampler used!"); return true; } };
+ * 
+ * }
+ * 
+ * }
+ */
