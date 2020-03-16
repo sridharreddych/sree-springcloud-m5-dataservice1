6fe21b626abba3be283e85d261564e358bdc626e
SridharChallabotla <sridhar.challabotla@metlife.com>
2020-03-16 11:44:26 +0530

    first commit of dataservice1

---
+++ src/test/java/sree/demo/SreeSpringcloudM5Dataservice3ApplicationTests.java
@@ -0,0 +1,13 @@
+package sree.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SreeSpringcloudM5Dataservice3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
